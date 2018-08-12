ff0fbdb53359d439f0e912cc88e5ce00ea57b503
Basil Ossandon <basilossandon@gmail.com>
2018-08-11 23:54:36 -0400

    css update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
