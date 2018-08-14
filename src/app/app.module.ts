2882ef8bb5461d43874c7515679d787b7287ecd9
Salazarkss <sebastian.salazar.v@usach.cl>
2018-08-13 23:18:14 -0300

    Borrado de carpeta + cambio app.html y app.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
- import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { IdeasComponent } from './ideas/ideas.component';
- @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    IdeasComponent
-=======
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { IdeasComponent } from './ideas/ideas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
->>>>>>> abec7bec95dc1c12c627de0ba34b1b0d7aa16208
+    NavbarComponent,
+    IdeasComponent
   ],
   imports: [
     BrowserModule
@@ -26,8 +16,4 @@ import { NavbarComponent } from './navbar/navbar.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-<<<<<<< HEAD
 export class AppModule { }
-=======
-export class AppModule { }
->>>>>>> abec7bec95dc1c12c627de0ba34b1b0d7aa16208
